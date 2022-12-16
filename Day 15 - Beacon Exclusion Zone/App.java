4c0eb09659ac94c2106a1d5ab2c7830e3125b394
Josh Sharp <jsharp9009@live.com>
2022-12-15 22:13:24 -0600

    switching machines

--- Day 15 - Beacon Exclusion Zone/App.java
+++ Day 15 - Beacon Exclusion Zone/App.java
@@ -1,9 +1,11 @@
+import java.awt.Point;
 import java.io.File;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.regex.Pattern;
+import java.util.stream.Collectors;
 
 public class App {
     static Pattern pattern = Pattern.compile("\\d+");
@@ -12,21 +14,69 @@ public class App {
         var file = new File("input.txt");
         var input = Files.readAllLines(Paths.get(file.getAbsolutePath()));
         var senors = ParseInput(input);
+        SolvePartOne(senors, 2000000);
     }
 
     static ArrayList<Sensor> ParseInput(List<String> input) {
         var result = new ArrayList<Sensor>();
         for (String line : input) {
             var matcher = pattern.matcher(line);
-            if (matcher.matches()) {
-                var sX = Integer.parseInt(matcher.group(0));
-                var sY = Integer.parseInt(matcher.group(1));
-                var bX = Integer.parseInt(matcher.group(2));
-                var bY = Integer.parseInt(matcher.group(3));
-                result.add(new Sensor(sX, sY, bX, bY));
+            Integer sX = 0;
+            Integer sY = 0;
+            Integer bX = 0;
+            Integer bY = 0;
+            var count = 0;
+            while (matcher.find()) {
+                switch(count){
+                    case 0: sX = Integer.parseInt(matcher.group()); count++; break;
+                    case 1: sY = Integer.parseInt(matcher.group()); count++; break;
+                    case 2: bX = Integer.parseInt(matcher.group()); count++; break;
+                    case 3: bY = Integer.parseInt(matcher.group()); count++; break;
+                }
             }
-
+            result.add(new Sensor(sX, sY, bX, bY));
         }
         return result;
+    }
+
+    static void SolvePartOne(ArrayList<Sensor> sensors, Integer line){
+        var canTouch = (Sensor[])sensors.stream().filter(p -> p.CanSee(p.x, line)).toArray(Sensor[]::new);
+        var beacons = sensors.stream().filter(p -> p.beacon.y == line).distinct().collect(Collectors.toList());
+        ArrayList<Point> checked = new ArrayList<Point>();
+        for (Sensor sensor : canTouch) {
+            if(!checked.stream().anyMatch(p -> p.x == sensor.x && p.y == line)
+            && !beacons.stream().anyMatch(p -> p.beacon.x == sensor.x && p.beacon.y == line))
+                checked.add(new Point(sensor.x, line));
+            
+            var x = sensor.x + 1;
+            while(true){
+                var checkX = x;
+                if(sensor.CanSee(new Point(x, line)) 
+                && !checked.stream().anyMatch(p -> p.x == checkX && p.y == line)
+                && !beacons.stream().anyMatch(p -> p.beacon.x == checkX && p.beacon.y == line)){
+                    checked.add(new Point(x, line));
+                    x++;
+                }
+                else{
+                    break;
+                }
+            }
+
+            x = sensor.x - 1;
+            while(true){
+                var checkX = x;
+                if(sensor.CanSee(new Point(x, line)) 
+                && !checked.stream().anyMatch(p -> p.x == checkX && p.y == line)
+                && !beacons.stream().anyMatch(p -> p.beacon.x == checkX && p.beacon.y == line)){
+                    checked.add(new Point(x, line));
+                    x--;
+                }
+                else{
+                    break;
+                }
+            }
+        }
+        //checked.stream().forEach(s -> System.out.println(s));
+        System.out.println("Part One - space scanned: " + checked.stream().distinct().count());
     }
 }
--- Day 15 - Beacon Exclusion Zone/Sensor.java
+++ Day 15 - Beacon Exclusion Zone/Sensor.java
@@ -2,21 +2,32 @@ import java.awt.Point;
 
 public class Sensor extends Point {
     Point beacon;
-
+    Integer distance;
     public Sensor(int x, int y, Integer beaconX, Integer beaconY) {
         super(x, y);
         this.beacon = new Point(beaconX, beaconY);
+        this.distance = CalculateDistance(beacon.x, beacon.y, this.x, this.y);
     }
 
     public Point getBeacon() {
         return beacon;
     }
 
-    public void setBeacon(Point beacon) {
-        this.beacon = beacon;
+    public Integer GetDistance(){
+        return distance;
     }
 
-    public Integer GetDistance(){
-        return Math.abs(beacon.x - this.x) + Math.abs(beacon.y - this.y);
+    public static Integer CalculateDistance(Integer p1x, Integer p1y, Integer p2x, Integer p2y){
+        return Math.abs(p1x - p2x) + Math.abs(p1y - p2y);
+    }
+
+    public boolean CanSee(Point point){
+        var dist = CalculateDistance(this.x, this.y, point.x, point.y);
+        return dist <= distance;
+    }
+
+    public boolean CanSee(Integer p1x, Integer p1y){
+        var dist = CalculateDistance(p1x, p1y, this.x, this.y);
+        return dist < distance;
     }
 }
